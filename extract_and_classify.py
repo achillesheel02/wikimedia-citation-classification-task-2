1cfb6a734bbef9b426ba7a9ff5d59840e21bf8f9
Barak <bachillah@gmail.com>
2019-10-06 00:45:23 +0300

    tried using mwpasrerfromhell library

--- extract_and_classify.py
+++ extract_and_classify.py
@@ -1,1 +1,24 @@
-print "Hello!"
+# import statements
+import mwapi
+import mwparserfromhell
+
+title_query = input()
+session = mwapi.Session('https://en.wikipedia.org')
+
+response = session.get(
+    action="query",
+    list="search",
+    format="json",
+    srsearch=title_query
+
+)
+for item in response['query']['search']:
+    print("{title}\n{pageid}\n......................................\n\n".format(**item))
+    content = session.get(
+        action="parse",
+        pageid=int("{pageid}".format(**item)),
+        format="json"
+    )
+    wikicode = mwparserfromhell.parse(content['parse']['text']['*'])
+    print(wikicode.filter_text())
+    break
